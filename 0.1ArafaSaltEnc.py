daefa5d1f836e521966099a16354e5bcbd4a35c3
0.1Arafa <86897733+0Arafa@users.noreply.github.com>
2022-01-29 10:27:42 +0200

    Update 0.1ArafaSaltEnc.py

--- 0.1ArafaSaltEnc.py
+++ 0.1ArafaSaltEnc.py
@@ -1,10 +1,10 @@
-#!usr/bin/python
+#!usr/bin/python3
 
-import crypt
+from crypt import crypt
 
 print("\t"*4,"#### By: Abd Almoen Arafa (0.1Arafa) ####"),print("\t"*4,"#### Age: 15                          ####\n")
 text=input("Enter The String to salt it: ")
 Text=input("Enter The Letters to salt: ")
-print(crypt.crypt(text,Text),"\n")
+print(crypt(text,Text),"\n")
 #By: Abd Almoen Arafa (0.1Arafa)
 #Age: 15
